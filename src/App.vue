8eacdcb3d7bf60748faa86095e7d96de2ee2eaf7
Tom <tom@pop-os.localdomain>
2020-09-05 15:28:56 -0500

    init vue project with hello world connect

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      {{ title }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: "This is a vue Quiz"
+      }
+    }
+  }
+</script>
+<style scoped>
+  div { 
+    background-color: green;
+  }
+</style>
